2aebe0f03f761d35a2019a8c9cd2b4acd7aa4c1b
ofer <ofer@aviator.co>
2026-07-21 16:56:35 -0700

    refresh managed tool configs

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,19 +1,52 @@
 # homebrew first
 /opt/homebrew/bin/brew shellenv | source
 
-set -gx XDG_CONFIG_HOME $HOME/.config
+fish_add_path (go env GOPATH)/bin
 
 set fish_greeting
 
-abbr --add brewu 'brew update && brew upgrade'
-
-alias claude="/Users/ofergoldstein/.claude/local/claude"
+set -gx XDG_CONFIG_HOME $HOME/.config
 
 set -gx EDITOR "code --wait"
 set -gx VISUAL "code --wait"
 
+set -gx HOMEBREW_NO_ASK 1
+
 string match -q "$TERM_PROGRAM" "vscode"
 and . (code --locate-shell-integration-path fish)
+
+# adds alias for "kubectl" to "kubecolor" with completions
+function kubectl --wraps kubectl
+  command kubecolor $argv
+end
+
+# reuse "kubectl" completions on "kubecolor"
+function kubecolor --wraps kubectl
+  command kubecolor $argv
+end
+
+# yazi wrapper to change directory after running yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+alias ls='eza --all --long --group-directories-first --icons --git --header --no-user --hyperlink=auto'
+
+abbr --add dev 'cd ~/dev'
+
+abbr --add brewu 'brew update && brew upgrade'
+
+abbr --add k 'kubectl'
+
+abbr --add python "ipython"
+abbr --add python3 "ipython"
+
+abbr --add cat 'bat'
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
@@ -26,27 +59,6 @@ starship init fish | source
 
 direnv hook fish | source
 
-# adds alias for "kubectl" to "kubecolor" with completions
-function kubectl --wraps kubectl
-  command kubecolor $argv
-end
-
-# reuse "kubectl" completions on "kubecolor"
-function kubecolor --wraps kubectl
-  command kubecolor $argv
-end
-
-alias ls='eza --all --long --group-directories-first --icons --git --header --no-user --hyperlink'
-
-abbr --add k 'kubectl'
-
-abbr --add dev 'cd ~/dev'
-
-abbr --add python "ipython"
-abbr --add python3 "ipython"
-
-abbr --add htopu 'htop -u (whoami)'
-
-abbr --add cat 'bat'
-
 zoxide init fish | source
+
+set -gx PATH "$HOME/.local/bin" $PATH
